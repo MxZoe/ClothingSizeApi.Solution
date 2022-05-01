3283c82e27fa4f2662490c988c1605080b55a046
Zoe Weinstein <mxzweinstein@gmail.com>
2022-05-01 11:09:31 -0600

    fix search info

--- ClothingSizeApi/Controllers/BrandsController.cs
+++ ClothingSizeApi/Controllers/BrandsController.cs
@@ -22,9 +22,66 @@ namespace ClothingSizeApi.Controllers
 
     // GET: api/Brands/
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Brand>>> Get()
+    public async Task<ActionResult<IEnumerable<Brand>>> Get(string name, string gender, string clothingType, string xxxs, string xxs, string xs, string s, string m, string l, string xl, string xxl, string xxxl, string xxxxl)
     {
-      return await _db.Brands.ToListAsync();
+      var query = _db.Brands.AsQueryable();
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+
+      if (clothingType != null)
+      {
+        query = query.Where(entry => entry.ClothingType == clothingType);
+      }    
+        if (xxxs !=null)
+      {
+        query = query.Where(entry => entry.XXXS == xxxs);
+      }   
+  
+      if (xxs !=null)
+      {
+        query = query.Where(entry => entry.XXS == xxs);
+      }  
+
+      if (xs !=null)
+      {
+        query = query.Where(entry => entry.XS == xs);
+      }  
+     if (s !=null)
+      {
+        query = query.Where(entry => entry.S == s);
+      }  
+     if (m !=null)
+      {
+        query = query.Where(entry => entry.M == m);
+      } 
+     if (l !=null)
+      {
+        query = query.Where(entry => entry.L == l);
+      } 
+     if (xl !=null)
+      {
+        query = query.Where(entry => entry.XL == xl);
+      } 
+     if (xxl !=null)
+      {
+        query = query.Where(entry => entry.XXL == xxl);
+      } 
+     if (xxxl !=null)
+      {
+        query = query.Where(entry => entry.XXXL == xxxl);
+      } 
+      if (xxxxl !=null)
+      {
+        query = query.Where(entry => entry.XXXXL == xxxxl);
+      } 
+      return await query.ToListAsync();
       
     }
 
@@ -105,8 +162,8 @@ namespace ClothingSizeApi.Controllers
       return _db.Brands.Any(e => e.BrandId == id);
     }
 
-    // GET: api/Brands/Name/
-    [HttpGet("/Name")]
+    // GET: api/Brands/gap/
+    [HttpGet("/{name}")]
     public async Task<ActionResult<IEnumerable<Brand>>> GetName(string name)
     {
       var query = _db.Brands.AsQueryable();
@@ -119,49 +176,49 @@ namespace ClothingSizeApi.Controllers
       return await query.ToListAsync();
     }
 
-    // GET: api/Brands/size/
-    [HttpGet("/size")]
-    public async Task<ActionResult<IEnumerable<Brand>>> GetSize(string size)
-    {
-      var query = _db.Brands.AsQueryable();
-
-     switch (size)
+    // GET: api/Brands/
+     [HttpGet("/brands/")]
+     public async Task<ActionResult<IEnumerable<Brand>>> GetSize(string size)
      {
-        case "xxxs":
-          query = query.Where(entry => entry.XXXS == size);
-          break;
-        case "xxs":
-          query = query.Where(entry => entry.XXS == size);
-          break;
-        case "xs":
-          query = query.Where(entry => entry.XS == size);
-          break;
-        case "s":
-          query = query.Where(entry => entry.S == size);
-          break;
-        case "M":
-          query = query.Where(entry => entry.M == size);
-          break;
-        case "L":
-          query = query.Where(entry => entry.L == size);
-          break;
-        case "XL":
-          query = query.Where(entry => entry.XL == size);
-          break;
-        case "XXL":
-          query = query.Where(entry => entry.XXL == size);
-          break;
-        case "XXXL":
-          query = query.Where(entry => entry.XXXL == size);
-          break;
-        case "XXXXL":
-          query = query.Where(entry => entry.XXXXL == size);
-          break;
+       var query = _db.Brands.AsQueryable();
+
+      switch (size)
+      {
+         case "xxxs":
+           query = query.Where(entry => entry.XXXS == size);
+           break;
+         case "xxs":
+           query = query.Where(entry => entry.XXS == size);
+           break;
+         case "xs":
+           query = query.Where(entry => entry.XS == size);
+           break;
+         case "s":
+           query = query.Where(entry => entry.S == size);
+           break;
+         case "M":
+           query = query.Where(entry => entry.M == size);
+           break;
+         case "L":
+           query = query.Where(entry => entry.L == size);
+           break;
+         case "XL":
+           query = query.Where(entry => entry.XL == size);
+           break;
+         case "XXL":
+           query = query.Where(entry => entry.XXL == size);
+           break;
+         case "XXXL":
+           query = query.Where(entry => entry.XXXL == size);
+           break;
+         case "XXXXL":
+           query = query.Where(entry => entry.XXXXL == size);
+           break;
+      }
+
+
+       return await query.ToListAsync();
      }
-
-
-      return await query.ToListAsync();
-    }
     
   }
 }
